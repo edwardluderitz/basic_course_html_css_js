800ab20c4209e0a20f2bb7543f9a03db951186f7
Edward Lalbem Lüderitz <edluder@gmail.com>
2022-06-15 23:20:33 -0300

    added a box that shows information about how the calculation works and some more functionalities

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+function infosobre(){
+    let contentInfo = document.getElementById("textinfocalc")
+    let textfirstInfo = document.getElementById("textfirst")
+    let sobreResult = document.getElementById("sobre-result");
+    textfirstInfo.innerHTML = "Como é feito o cálculo:"
+    contentInfo.innerHTML = "O cálculo básico para se descobrir se o álcool é vantajoso ou não em relação à gasolina é simples. Basta dividir o preço do litro do álcool pelo da gasolina. Se o resultado for inferior a 0,7, use álcool. Se for maior que 0,7, então a gasolina é melhor.";
+    sobreResult.classList.remove("hide");
+}
+
+function infosobresair(){
+    let sobreResult = document.getElementById("sobre-result");
+    sobreResult.classList.add("hide");
+}
+
 function calcular(event){
     event.preventDefault();
 
@@ -19,3 +33,5 @@ function calcular(event){
 
     contentResult.classList.remove("hide");
 }
+
+
